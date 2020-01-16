cea0cca1619e2ecacc0b078331e6f4ba594fc8d7
Alice Rose <alice@acolorbright.com>
2020-01-16 17:36:56 +0100

    feat: Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: [
+    "stylelint-config-recommended",
+    "stylelint-config-property-sort-order-acb",
+  ],
+  rules: {
+    "at-rule-no-unknown": [true, { "ignoreAtRules": ["define-mixin", "mixin"]}],
+    "at-rule-no-vendor-prefix": true,
+    "color-hex-case": "lower",
+    "color-hex-length": "short",
+    "color-no-invalid-hex": true,
+    "max-nesting-depth": 3,
+    "media-feature-name-no-vendor-prefix": true,
+    "value-no-vendor-prefix": true,
+    "declaration-property-value-whitelist": {
+      "/color$/": [
+        "/^\\$|initial|inherit|transparent|currentColor|gray|hwb|rgba|hsl|var/"
+      ],
+      "fill": [
+        "/^\\$|initial|inherit|transparent|currentColor|gray|hwb|rgba|hsl|var/"
+      ],
+      "stroke": [
+        "/^\\$|initial|inherit|transparent|currentColor|gray|hwb|rgba|hsl|var/"
+      ]
+    }
+  }
+}
